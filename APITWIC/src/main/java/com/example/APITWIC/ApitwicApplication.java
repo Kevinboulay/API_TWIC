2c446976dd68ac2384e89249637e5eab83faf408
Kevin_Boulay <kevin.boulay@reseau.eseo.fr>
2023-06-20 17:16:17 +0200

    Setup

---
+++ APITWIC/src/main/java/com/example/APITWIC/ApitwicApplication.java
@@ -0,0 +1,13 @@
+package com.example.APITWIC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitwicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitwicApplication.class, args);
+	}
+
+}
---
+++ APITWIC/src/test/java/com/example/APITWIC/ApitwicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APITWIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitwicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
